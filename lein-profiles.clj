c0ab47f9d123cfb624f158068fe42889623b2652
Sasha Gerrand <sgerrand@users.noreply.github.com>
2017-11-24 19:54:00 +0000

    :nail_care: Update dependencies

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,8 +1,8 @@
 {:user {:plugins [[org.clojure/tools.nrepl "0.2.11"]
                   [cider/cider-nrepl "0.15.1"]
-                  [jonase/eastwood "0.2.3"]
-                  [lein-ancient "0.6.10"]
-                  [lein-cljfmt "0.3.0"]
-                  [lein-cloverage "1.0.6"]
-                  [lein-kibit "0.1.2"]
-                  [test2junit "1.2.1"]]}}
+                  [jonase/eastwood "0.2.5"]
+                  [lein-ancient "0.6.14"]
+                  [lein-cljfmt "0.5.7"]
+                  [lein-cloverage "1.0.10"]
+                  [lein-kibit "0.1.6"]
+                  [test2junit "1.3.3"]]}}
